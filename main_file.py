5d33495ca23cdee77bf8f7c0a3f9a7bbda8716a6
semarillion <anonymous@mailbox.org>
2021-11-09 16:39:14 +0100

    new comment

--- main_file.py
+++ main_file.py
@@ -1,16 +1,1 @@
-print('Hello')
-
-# dev1
-print('This ist the second line')
-
-# dev1
-print('this is the second line')
-
-# dev1
-print('this is the third line')
-
-# -- dev1 --
-print(' new line with new dev1')
-#
-#
-##
+# new comment
